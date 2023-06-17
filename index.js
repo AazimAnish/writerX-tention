4316f5fc5df810dae0de43a7c937c09666839310
AazimAnish <aazimanish1@gmail.com>
2023-06-17 23:49:10 +0530

    save key added

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ document.getElementById('save_key_button').addEventListener('click', saveKey);
 document
   .getElementById('change_key_button')
   .addEventListener('click', changeKey);
+
+  const saveKey = () => {}
+
+const changeKey = () => {}
+
+document.getElementById('save_key_button').addEventListener('click', saveKey);
+document
+  .getElementById('change_key_button')
+  .addEventListener('click', changeKey);
