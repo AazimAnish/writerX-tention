c40b526434f59622c53f58c982f0210dd146b65a
AazimAnish <aazimanish1@gmail.com>
2023-06-19 00:00:00 +0530

    check for key

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const checkForKey = () => {
+  return new Promise((resolve, reject) => {
+    chrome.storage.local.get(['openai-key'], (result) => {
+      resolve(result['openai-key']);
+    });
+  });
+};
+
 document.getElementById('save_key_button').addEventListener('click', saveKey);
 document
   .getElementById('change_key_button')
@@ -33,5 +41,12 @@ document.getElementById('save_key_button').addEventListener('click', saveKey);
 document
   .getElementById('change_key_button')
   .addEventListener('click', changeKey);
+
+  checkForKey().then((response) => {
+    if (response) {
+      document.getElementById('key_needed').style.display = 'none';
+      document.getElementById('key_entered').style.display = 'block';
+    }
+  });
 
   
